bc4264742ef50a04a42fd9d4a21d62f6e273713f
martingrzzler <martin.gressler@code.berlin>
2021-06-03 17:56:22 +0200

    intial commit

---
+++ background.js
@@ -0,0 +1,18 @@
+// firefox uses browser
+const browser = chrome ?? browser
+
+browser.webRequest.onHeadersReceived.addListener(
+  (info) => {
+    const headers = info.responseHeaders.filter(header =>
+      header.name.toLowerCase() !== 'x-frame-options' && header.name.toLowerCase() !== 'content-security-policy'
+    )
+    return { responseHeaders: headers }
+  }, {
+  urls: ['<all_urls>'],
+  types: ['sub_frame']
+}, [
+  'blocking',
+  'responseHeaders',
+  chrome.webRequest.OnHeadersReceivedOptions.EXTRA_HEADERS
+]
+)
