6f3b982e2457d23dc890ce29cc02733a80fefe79
martingrzzler <martin.gressler@code.berlin>
2021-07-08 13:10:13 +0200

    made extension work only for coopsaas and localhost

--- background.js
+++ background.js
@@ -1,19 +1,26 @@
 // firefox uses browser
-const browser = chrome ?? browser
+const browser = chrome ?? browser;
+const INITIATORS = new Set(['http://localhost:3000', 'https://coopsaas.com'])
 
 browser.webRequest.onHeadersReceived.addListener(
   (info) => {
-    const headers = info.responseHeaders.filter(header =>
-      // content-security-policy can also block iframes 
-      header.name.toLowerCase() !== 'x-frame-options' && header.name.toLowerCase() !== 'content-security-policy'
-    )
-    return { responseHeaders: headers }
-  }, {
-  urls: ['<all_urls>'],
-  types: ['sub_frame']
-}, [
-  'blocking',
-  'responseHeaders',
-  chrome.webRequest.OnHeadersReceivedOptions.EXTRA_HEADERS
-]
-)
+    if (!INITIATORS.has(info.initiator)) return
+    const headers = info.responseHeaders.filter((header) => {
+      // content-security-policy can also block iframes
+      return (
+        header.name.toLowerCase() !== "x-frame-options" &&
+        header.name.toLowerCase() !== "content-security-policy"
+      );
+    });
+    return { responseHeaders: headers };
+  },
+  {
+    urls: ["<all_urls>"],
+    types: ["sub_frame"],
+  },
+  [
+    "blocking",
+    "responseHeaders",
+    chrome.webRequest.OnHeadersReceivedOptions.EXTRA_HEADERS,
+  ]
+);
