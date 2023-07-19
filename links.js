551bc11f20a5677c134c1aeb2747bea89e86c1fa
Martin Gressler <martin.gressler@code.berlin>
2023-07-19 10:25:44 -0500

    add domains

--- links.js
+++ links.js
@@ -1,16 +1,33 @@
 chrome.storage.local.get('links', function (data) {
   const links = data.links || []
-  const ul = document.getElementById('links')
+  const ulLinks = document.getElementById('links')
+  const ulDomains = document.getElementById('domains')
   const header = document.getElementById('header')
   header.innerText = `Found ${links.length} outbound links`
 
   links.forEach(function (link) {
-    const li = document.createElement('li')
-    const a = document.createElement('a')
-    a.href = link
-    a.innerText = link
-    a.target = '_blank'
-    li.appendChild(a)
-    ul.appendChild(li)
+    appendLine(ulLinks, link)
+    appendLine(ulDomains, getDomain(link))
   })
 })
+
+function appendLine(container, line) {
+  const li = document.createElement('li')
+  const a = document.createElement('a')
+  a.href = line
+  a.innerText = line
+  a.target = '_blank'
+  li.appendChild(a)
+  container.appendChild(li)
+}
+
+function getDomain(url) {
+  const urlObj = new URL(url)
+  let domain = urlObj.hostname
+
+  if (domain.startsWith('www.')) {
+    domain = domain.replace('www.', '')
+  }
+
+  return domain
+}
