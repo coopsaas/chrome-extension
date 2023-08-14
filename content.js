41d93521e3debde9bd3c6b6d79192c3c74a147d6
Martin Gressler <martin.gressler@code.berlin>
2023-08-14 12:04:59 -0500

    implement automatic scrolling

--- content.js
+++ content.js
@@ -1,7 +1,8 @@
-let intervalId = null
+let intervalIds = []
 const outboundLinks = new Set()
+let scrollElement = null
 
-const extractOutboundLinks = () => {
+function extractOutboundLinks() {
   const links = document.getElementsByTagName('a')
   for (const link of links) {
     const href = link.href
@@ -11,21 +12,56 @@ const extractOutboundLinks = () => {
   }
 }
 
-function hoverEffect(e) {
-  e.target.style.border = '1px solid red'
+function mousOverEffect(e) {
+  e.target.style.border = '2px solid red'
+  e.target.style.cursor = 'pointer'
+}
+
+function mouseOutEffect(e) {
+  e.target.style.border = ''
+  e.target.style.cursor = ''
+}
+
+function setScrollElement(e) {
+  scrollElement = e.target
+  scrollElement.style.border = '2px solid red'
+  removeListeners()
+}
+
+function scroll() {
+  scrollElement.scrollBy({
+    top: 300,
+    behavior: 'smooth',
+  })
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'startExtracting') {
-    extractOutboundLinks()
-    intervalId = setInterval(extractOutboundLinks, 200)
+    intervalIds.push(setInterval(extractOutboundLinks, 200))
+
+    if (scrollElement) {
+      intervalIds.push(setInterval(scroll, 1000))
+    }
+
     sendResponse({ message: 'started' })
   } else if (request.message === 'stopExtracting') {
-    clearInterval(intervalId)
+    intervalIds.forEach((intervalId) => clearInterval(intervalId))
     sendResponse({ links: Array.from(outboundLinks) })
   } else if (request.message === 'enableSelect') {
-    document.addEventListener('mouseover', hoverEffect)
+    document.addEventListener('mouseover', mousOverEffect)
+    document.addEventListener('mouseout', mouseOutEffect)
+    document.addEventListener('click', setScrollElement)
   } else if (request.message === 'disableSelect') {
-    document.removeEventListener('mouseover', hoverEffect)
+    removeListeners()
+    if (scrollElement) {
+      scrollElement.style.border = ''
+      scrollElement = null
+    }
   }
 })
+
+function removeListeners() {
+  document.removeEventListener('mouseover', mousOverEffect)
+  document.removeEventListener('mouseout', mouseOutEffect)
+  document.removeEventListener('click', setScrollElement)
+}
